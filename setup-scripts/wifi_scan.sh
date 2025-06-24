ffb67040709b9a26977a37f52c9d0ae545a90bde
AlonHaim <alonhaim2003@gmail.com>
2025-06-24 16:59:19 +0300

    Create wifi_scan.sh

---
+++ setup-scripts/wifi_scan.sh
@@ -0,0 +1,15 @@
+#In this .sh file, are the commands I use in order to scan quickly Wifi networks and find their BSSID for further use.
+
+#this file is written for a network card called "wlan0" but you can change it to what ever your network card is called
+
+# Making the network card use monitor mode:
+ifconfig wlan0 down 
+airmon-ng check kill
+iwconfig wlan0 mode monitor
+ifconfig wlan0 up
+
+#Scanning networks through wlan0 on monitor mode:
+airodump-ng wlan0
+
+#for further use and selecting your desired network, do:
+#airodump-ng --bssid {wifi bssid} --channel {wifi channel} wlan0
